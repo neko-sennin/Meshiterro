0f5d1171f4866cd3b83811f9b674b0f29e8815b0
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-07-04 19:12:37 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  
+  root to: "homes#top"
+  devise_for :users
+  get 'homes/about' => 'homes#about',as: 'about'
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
+end
